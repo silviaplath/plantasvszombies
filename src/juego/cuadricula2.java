5577a10c4203e8d8042f7e6a925607bf584fae91
Abril <abrilvalentinaherrera@gmail.com>
2025-10-29 00:00:42 -0300

    l

--- src/juego/cuadricula2.java
+++ src/juego/cuadricula2.java
@@ -14,26 +14,22 @@ import entorno.InterfaceJuego;
 public class cuadricula2 {
 
 	Image img;
-    private  double filas;
-    private double columnas =5;
-    private double tamceldas = 100;
-    private double margensup = 100;
+    private  int filas=5;
+    private int columnas =9;
+    private double tamceldas;
+    private double margensup = 160;
     private double[] coorX, coorY;
     Image pasto1, pasto2;
     Entorno e;
-    private double escala = 0.5;
    
 public cuadricula2 (double x , double y , Entorno e) {
-	this.filas = Math.floor((e.alto() - margensup) / tamceldas);
+	
+	 this.e = e;
+	
+	tamceldas = Math.min(e.ancho() /(double) columnas, (e.alto()- margensup) / (double) filas);
 	this.coorX=new double [(int)columnas];
 	this.coorY= new double [(int)filas];
 	
-    this.e = e;
-
-    this.pasto1 = Herramientas.cargarImagen("pasto1.jpeg");
-    this.pasto2 = Herramientas.cargarImagen("pasto2.jpg");
-    
-
    for (int i =0; i< columnas; i++) {
 	   coorX[i]= i * tamceldas + tamceldas / 2;              
    }
@@ -41,7 +37,8 @@ public cuadricula2 (double x , double y , Entorno e) {
 	   coorY[j] = margensup + tamceldas / 2 + j * tamceldas; 
    }
 
-    
+   this.pasto1 = Herramientas.cargarImagen("pasto1.jpeg");
+   this.pasto2 = Herramientas.cargarImagen("pasto2.jpg");
 
     
 
@@ -51,7 +48,11 @@ public void dibujar () {
     for (int i = 0; i < coorY.length; i++) {
         for (int j = 0; j < coorX.length; j++) {
         	 Image img = ((i + j) % 2 == 0) ? pasto1 : pasto2;
-            e.dibujarImagen(img, coorX[j], coorY[i], 0, escala);
+        	 
+        	 double escalaX = tamceldas / img.getWidth(null);
+             double escalaY = tamceldas / img.getHeight(null);
+             double escalaFinal = Math.min(escalaX, escalaY);
+            e.dibujarImagen(img, coorX[j], coorY[i], 0, escalaFinal);
         }
     }
 
